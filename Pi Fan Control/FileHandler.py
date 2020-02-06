044fae516c7cba0fafef9818e8ad9af6fca66eec
Gabe <gabezter4@yahoo.com>
2020-02-06 15:43:51 -0600

    Added basic methods

---
+++ Pi Fan Control/FileHandler.py
@@ -0,0 +1,68 @@
+'''
+Created on Feb 6, 2020
+
+@author: gabez
+'''
+from datetime import datetime as __datetime
+import json
+
+__json_fileR = open("hosts.json", "r")
+__host_file = json.load(__json_fileR)
+
+
+def getHostName(ip):
+    if str(ip) in __host_file:
+        name = __host_file[str(ip)]["hostname"]
+        return name
+    else:
+        return False
+
+
+def addHostName(ip, hostname, temp):
+    if(str(ip) not in __host_file):
+        time = __datetime.now().strftime('%Y-%m-%d %H-%M-%S')
+        __host_file[str(ip)] = {
+            "hostname": str(hostname),
+            "address": str(ip),
+            "last report": time,
+            "temperature": temp
+        }
+        __saveFile()
+        return True
+    else:
+        return False
+
+
+def setHostName(ip, hostname):
+    if str(ip) in __host_file:
+        if not checkHostName(ip, hostname):
+            __host_file[str(ip)]["name"] = hostname
+            __saveFile()
+            return True
+    else:
+        return False
+
+
+def checkHostName(ip, hostname):
+    stored_hostname = getHostName(ip)
+    if (hostname.lower() == stored_hostname.lower()):
+        return True
+    else:
+        return False
+
+
+def __saveFile():
+    json_fileW = open("hosts.json", 'w')
+    json_fileW.write(json.dump(__host_file, indent=4))
+    json_fileW.close()
+    return True
+
+
+def update(ip, time, temp):
+    if(str(ip) in __host_file):
+        __host_file[str(ip)]["last report"] = time
+        __host_file[str(ip)]["temperature"] = temp
+        __saveFile()
+        return True
+    else:
+        return False
--- Pi Fan Control/Server.py
+++ Pi Fan Control/Server.py
@@ -5,16 +5,46 @@ Created on Feb 6, 2020
 '''
 
 import socketserver
-from http import server
+import json
+import logging
+import datetime
+import http.server
+from FileHandler import *
+
 
 class Server(object):
 
     def __init__(self, params):
         self.run()
-        
+
     def run(self):
         pass
+
     def listen(self):
-        pass 
+        pass
+
     def post(self):
         pass
+
+
+class ThreadedTCPRequestHandler(socketserver.StreamRequestHandler):
+    def handle(self):
+        data = self.request.recv(1024).decode('utf-8')
+        addr = self.client_address
+        ip = addr[0]
+        info = json.load(data)
+        if(str(ip) == info["data"]["address"] and hash(info["data"]["date"], info["data"]["checksum"], info["data"]["code"])):
+            hostname = info["data"]["hostname"]
+            temp = info["data"]["temperature"]
+            if not getHostName(ip):
+                addHostName(ip, hostname, temp)
+            else:
+                if not checkHostName(ip, hostname):
+                    setHostName(ip, hostname)
+
+    def hash(self, date, checksum, code):
+        date_code = id(date)
+        size = len(date_code)/2
+
+        return True
+        return False
