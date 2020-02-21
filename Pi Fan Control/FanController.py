a2195bb0d507ec3c150b8340a7405e7d0e2a13f1
Gabe <gabezter4@yahoo.com>
2020-02-21 09:03:50 -0600

    Created basic functions for fan control

--- Pi Fan Control/FanController.py
+++ Pi Fan Control/FanController.py
@@ -4,14 +4,35 @@ Created on Feb 6, 2020
 @author: gabez
 '''
 
-class FanController(object):
-    '''
-    classdocs
-    '''
+import time
+import threading
+from FileHandler import getTempAvg
+
+__fans_running = False
 
 
-    def __init__(self, params):
-        '''
-        Constructor
-        '''
+def __init__():
+    while(True):
+        avg = getTempAvg()
         
+        if(avg >= 95):
+            if not __fans_running:
+                __start_fans()
+        else:
+            if __fans_running:
+                __stop_fans()
+#         time.sleep(60)
+
+
+def __start_fans():
+    print("Starting Fans")
+    return
+
+
+def __stop_fans():
+    print("Stopping Fans")
+    return
+
+
+fs = threading.Thread(targe=__init__())
+fs.setDaemon(True)
